867c67a78e7aa357324d88aa87591ed47a014735
Mehmet Eroglu <mehmet---eroglu@hotmail.com>
2017-02-03 10:11:01 +0300

    Initial Commit

---
+++ multiples-exerciseTests/multiples_exerciseTests.swift
@@ -0,0 +1,36 @@
+//
+//  multiples_exerciseTests.swift
+//  multiples-exerciseTests
+//
+//  Created by Mehmet Eroğlu on 3.02.2017.
+//  Copyright © 2017 Mehmet Eroğlu. All rights reserved.
+//
+
+import XCTest
+@testable import multiples_exercise
+
+class multiples_exerciseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
